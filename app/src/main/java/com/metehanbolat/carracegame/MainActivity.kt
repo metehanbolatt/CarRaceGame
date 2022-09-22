6d131c935d2388bb5b36717fa46acdea4046ee76
Metehan <metehan.bolat.ie@gmail.com>
2022-09-22 17:17:53 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/carracegame/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.metehanbolat.carracegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metehanbolat.carracegame.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
